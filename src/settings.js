d4ce665c8b54e680f05ea15ac9d33919042e0f05
施聪滚 <2378710901@qq.com>
2021-05-15 16:02:17 +0800

    项目初始化

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+  //项目标题
+  title: 'Vue Admin Template',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false, //头部固定
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false   //侧边栏logo显不显示
+}
